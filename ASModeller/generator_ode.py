2222662081ad9d090eeb818fa965c359a314d76f
Maurice HT Ling <mauriceling@acm.org>
2018-08-19 22:12:15 +0800

    + initial import - ASModeller

---
+++ ASModeller/generator_ode.py
@@ -0,0 +1,257 @@
+'''
+Functions to generate ODE script from model objects.
+
+Date created: 6th August 2018
+
+This file is part of AdvanceSyn Modeller, which is a part of 
+AdvanceSyn ToolKit.
+
+Copyright (c) 2018, AdvanceSyn Private Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License") for 
+academic and not-for-profit use only; commercial and/or for profit 
+use(s) is/are not licensed under the License and requires a 
+separate commercial license from the copyright owner (AdvanceSyn 
+Private Limited); you may not use this file except in compliance 
+with the License. You may obtain a copy of the License at 
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+import re
+from datetime import datetime
+
+def generate_object_table(objlist):
+    '''!
+    Function to generate a table of object numbering. from table 
+    of objects.
+
+    @param objlist Dictionary: Table of objects where key is the 
+    object name and value is the object numbering.
+    @return: Dictionary / table of objects where key is the object 
+    name and value is the object numbering.
+    '''
+    objTable = {}
+    count = 0
+    for key in objlist:
+        objTable[key] = count
+        count = count + 1
+    return objTable
+
+def substitute_rateEq(objlist, objTable):
+    '''!
+    Function to change / edit each rate equations for each node / 
+    entity to vector variable.
+
+    @param objlist Dictionary: Table of objects where key is the 
+    object name and value is the object numbering.
+    @param objTable Dictionary: Table of objects where key is the 
+    object name and value is the object numbering.
+    @return: Modified table of objects.
+    '''
+    sTable = {}
+    for k in objTable:
+        sTable[k] = 'y[%s]' % str(objTable[k])
+    for name in objlist: 
+        for item in objlist[name].influx:
+            for k in objTable:
+                objlist[name].influx[item] = \
+                    re.sub(r'\b%s\b' % k, sTable[k], 
+                           objlist[name].influx[item])
+        for item in objlist[name].outflux:
+            for k in objTable:
+                objlist[name].outflux[item] = \
+                    re.sub(r'\b%s\b' % k, sTable[k], 
+                           objlist[name].outflux[item])
+    return objlist
+
+def print_rateEq(objlist):
+    '''!
+    Function to generate the ODE function codes for each entity 
+    from table of objects.
+
+    @param objlist Dictionary: Table of objects where key is the 
+    object name and value is the object numbering.
+    @return: List of Python codes representing the ODE - one
+    element is one line.
+    '''
+    printList = []
+    for name in objlist:
+        rateEq_name = re.sub('\.', '_', name)
+        finalTerms = [[], []]
+        pTerm = 'def %s(t, y):' % rateEq_name
+        printList.append(pTerm)
+        for item in objlist[name].influx:
+            term = objlist[name].influx[item]
+            termName = re.sub('\.', '_', item)
+            pTerm = '    %s = %s' % (termName, term)
+            printList.append(pTerm)
+            finalTerms[0].append(termName)
+        for item in objlist[name].outflux:
+            term = objlist[name].outflux[item]
+            termName = re.sub('\.', '_', item)
+            pTerm = '    %s = %s' % (termName, term)
+            printList.append(pTerm)
+            finalTerms[1].append(termName)
+        if len(finalTerms[0]) == 0: finalTerms[0] = ['0']
+        if len(finalTerms[1]) == 0: finalTerms[1] = ['0']
+        finalTerms = [' + '.join(finalTerms[0]), 
+                      ' + '.join(finalTerms[1])]
+        finalTerms = ['(%s)' % finalTerms[0],
+                      ' - (%s)' % finalTerms[1]]
+        finalTerms = ''.join(finalTerms)
+        pTerm = '    return %s' % finalTerms
+        printList.append(pTerm)
+        printList.append(' ')
+    return printList
+
+def print_header(spec):
+    '''!
+    Function to generate / print the headers / comments for the 
+    ODE script.
+
+    @param spec Object: ConfigParser object containing the processed 
+    model - from model_access.modelspec_reader() or 
+    model_access.specobj_reader() functions.
+    @return: List of Python codes representing the ODE - one
+    element is one line.
+    '''
+    utcnow = datetime.utcnow()
+    dtstamp = '%s-%s-%s %s:%s:%s:%s' % (str(utcnow.year), 
+                                        str(utcnow.month), 
+                                        str(utcnow.day), 
+                                        str(utcnow.hour), 
+                                        str(utcnow.minute), 
+                                        str(utcnow.minute), 
+                                        str(utcnow.microsecond))
+    printList = ["''' -------------------------------------",
+                 'Python ODE script generated by ASModeller',
+                 '(a package in AdvanceSynToolKit)',
+                 '',
+                 'Date Time: %s' % dtstamp,
+                 '']
+    for key in spec['Identifiers']:
+        printList.append('%s: %s' % (str(key), 
+                         str(spec['Identifiers'][key])))
+    printList.append("------------------------------------- '''")
+    printList.append('')
+    return printList
+
+def print_Setup(objlist, objTable, solver='RK4', 
+                timestep=1, endtime=21600):
+    '''!
+    Function to generate / print ODE simulation setup codes.
+
+    @param objlist Dictionary: Table of objects where key is the 
+    object name and value is the object numbering.
+    @param objTable Dictionary: Table of objects where key is the 
+    object name and value is the object numbering.
+    @param solver String: Type of solver to use. Allowable types 
+    are 'Euler', 'Heun' (Runge-Kutta 2nd method or Trapezoidal), 
+    'RK3' (third order Runge-Kutta), 'RK4' (fourth order 
+    Runge-Kutta), 'RK38' (fourth order Runge-Kutta method, 3/8 rule), 
+    'CK4' (fourth order Cash-Karp), 'CK5' (fifth order Cash-Karp), 
+    'RKF4' (fourth order Runge-Kutta-Fehlberg), 'RKF5' (fifth 
+    order Runge-Kutta-Fehlberg), 'DP4' (fourth order Dormand-Prince), 
+    and 'DP5' (fifth order Dormand-Prince). Default = 'RK4'.  
+    @param timestep Float: Time step interval for simulation. 
+    Default = 1.0.
+    @param endtime Float: Time to end simulation - the simulation 
+    will run from 0 to end time. Default = 21600.
+    @return: List containing the Python ODE setup codes (one 
+    element = one line).
+    '''
+    # 1. Generate ode codes
+    odecode = [x[:-1] for x in open('ASModeller\\ode.py').readlines()]
+    printList = odecode[8:37]
+    if solver == 'Euler':
+        printList = printList + odecode[37:115]
+    if solver == 'Heun':
+        printList = printList + odecode[115:204]
+    if solver == 'RK3':
+        printList = printList + odecode[204:303]
+    if solver == 'RK4':
+        printList = printList + odecode[303:409]
+    if solver == 'RK38':
+        printList = printList + odecode[409:515]
+    if solver == 'CK4':
+        printList = printList + odecode[515:641]
+    if solver == 'CK5':
+        printList = printList + odecode[641:766]
+    if solver == 'RKF4':
+        printList = printList + odecode[766:892]
+    if solver == 'RKF5':
+        printList = printList + odecode[892:1020]
+    if solver == 'DP4':
+        printList = printList + odecode[1020:1158]
+    if solver == 'DP5':
+        printList = printList + odecode[1158:1296]
+    # 2. Generate ODE vector
+    pTerm = 'ODE = list(range(%s))' % len(objTable)
+    #print(pTerm)
+    printList.append(pTerm)
+    labels = ['time']
+    for eqName in objTable:
+        name = re.sub('\.', '_', eqName)
+        pTerm = 'ODE[%s] = %s' % (str(objTable[eqName]), str(name))
+        printList.append(pTerm)
+        labels.append(name)
+    printList.append(' ')
+    # 3. Generate y vector
+    pTerm = 'y = list(range(%s))' % len(objlist)
+    printList.append(pTerm)
+    for name in objTable:
+        obj = objlist[name]
+        pTerm = 'y[%s] = %s    # %s' % \
+            (str(objTable[name]), str(obj.value['initial']),
+             str(name))
+        printList.append(pTerm)
+    printList.append(' ')
+    # 4. Print labels
+    pTerm = 'labels = %s' % str(labels)
+    printList.append(pTerm)
+    printList.append(' ')
+    # 5. Generate ODE execution
+    pTerm = 'model = %s(ODE, 0.0, y, %s, %s)' % \
+        (str(solver), str(timestep), str(endtime))
+    printList.append(pTerm)
+    return printList
+
+def generate_ODE(spec, modelobj, solver, 
+                 timestep, endtime):
+    '''!
+    Function to generate Python ODE codes by wrapping up other 
+    generation functions.
+
+    @param spec Object: ConfigParser object containing the processed 
+    model - from model_access.modelspec_reader() or 
+    model_access.specobj_reader() functions.
+    @param modelobj Dictionary: Table of objects where key is the 
+    object name and value is the object numbering.
+    @param solver String: Type of solver to use. Allowable types 
+    are 'Euler', 'Heun' (Runge-Kutta 2nd method or Trapezoidal), 
+    'RK3' (third order Runge-Kutta), 'RK4' (fourth order 
+    Runge-Kutta), 'RK38' (fourth order Runge-Kutta method, 3/8 rule), 
+    'CK4' (fourth order Cash-Karp), 'CK5' (fifth order Cash-Karp), 
+    'RKF4' (fourth order Runge-Kutta-Fehlberg), 'RKF5' (fifth 
+    order Runge-Kutta-Fehlberg), 'DP4' (fourth order Dormand-Prince), 
+    and 'DP5' (fifth order Dormand-Prince). Default = 'RK4'.  
+    @param timestep Float: Time step interval for simulation. 
+    Default = 1.0.
+    @param endtime Float: Time to end simulation - the simulation 
+    will run from 0 to end time. Default = 21600.
+    @return: List containing the Python ODE codes (one element = 
+    one line).
+    '''
+    objTable = generate_object_table(modelobj)
+    modelobj = substitute_rateEq(modelobj, objTable)
+    ODEHeader = print_header(spec)
+    ODEList = print_rateEq(modelobj)
+    ODESetup = print_Setup(modelobj, objTable, solver, 
+                           timestep, endtime)
+    datalist = ODEHeader + ODEList + ODESetup
+    return datalist
---
+++ ASModeller/model_access.py
@@ -0,0 +1,235 @@
+'''
+Functions to read model specification file into a series of 
+model objects, and providing interfaces to model.
+
+Date created: 6th August 2018
+
+This file is part of AdvanceSyn Modeller, which is a part of 
+AdvanceSyn ToolKit.
+
+Copyright (c) 2018, AdvanceSyn Private Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License") for 
+academic and not-for-profit use only; commercial and/or for profit 
+use(s) is/are not licensed under the License and requires a 
+separate commercial license from the copyright owner (AdvanceSyn 
+Private Limited); you may not use this file except in compliance 
+with the License. You may obtain a copy of the License at 
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+from configparser import ConfigParser
+from configparser import BasicInterpolation
+from configparser import ExtendedInterpolation
+import os
+
+from .model_object import ModelObject
+
+def modelspec_reader(modelfile, mode='extended'):
+    '''!
+    Function using ConfigParser (in Python Standard Library) to 
+    read a AdvanceSyn model specification file.
+
+    @param modelfile String: Relative path to the model specification 
+    file.
+    @param mode String: Type of interpolation mode for ConfigParser. 
+    Default = 'extended'. Allowable values are 'extended' (for 
+    extended interpolation) and 'basic' (for basic interpolation).
+    @return: ConfigParser object.
+    '''
+    if mode == 'extended':
+        spec = ConfigParser(interpolation=ExtendedInterpolation(),
+                            allow_no_value=True,
+                            delimiters=('=', ':'),
+                            comment_prefixes=('#', ';'),
+                            inline_comment_prefixes=('#', ';'),
+                            empty_lines_in_values=True,
+                            strict=False)
+    if mode == 'basic':
+        spec = ConfigParser(interpolation=BasicInterpolation(),
+                            allow_no_value=True,
+                            delimiters=('=', ':'),
+                            comment_prefixes=('#', ';'),
+                            inline_comment_prefixes=('#', ';'),
+                            empty_lines_in_values=True,
+                            strict=False)
+    modelspec = open(modelfile).read()
+    spec.read_string(modelspec)
+    return spec
+
+def specobj_reader(specobj, mode='extended'):
+    '''!
+    Function using ConfigParser (in Python Standard Library) to 
+    read a AdvanceSyn model specification as a dictionary-type object.
+
+    @param specobj Dictionary: Model specification in a dictionary / 
+    dictionary-like format.
+    @param mode String: Type of interpolation mode for ConfigParser. 
+    Default = 'extended'. Allowable values are 'extended' (for 
+    extended interpolation) and 'basic' (for basic interpolation).
+    @return: ConfigParser object.
+    '''
+    if mode == 'extended':
+        spec = ConfigParser(interpolation=ExtendedInterpolation(),
+                            allow_no_value=True,
+                            delimiters=('=', ':'),
+                            comment_prefixes=('#', ';'),
+                            inline_comment_prefixes=('#', ';'),
+                            empty_lines_in_values=True,
+                            strict=False)
+    if mode == 'basic':
+        spec = ConfigParser(interpolation=BasicInterpolation(),
+                            allow_no_value=True,
+                            delimiters=('=', ':'),
+                            comment_prefixes=('#', ';'),
+                            inline_comment_prefixes=('#', ';'),
+                            empty_lines_in_values=True,
+                            strict=False)
+    spec.read_dict(specobj)
+    return spec
+
+def generate_object_list_1(spec):
+    '''!
+    Function to generate a table (dictionary) of objects, which 
+    represents the entities / nodes of the model. This function 
+    is used for AdvanceSyn model specification type 1.
+
+    @param spec Object: ConfigParser object containing the processed 
+    model - from modelspec_reader() or specobj_reader() functions.
+    @return: Dictionary of objects where key is the object name 
+    and value is the object.
+    '''
+    names = [name for name in spec['Objects']]
+    descriptions = [spec['Objects'][name] for name in names]
+    objectlist = {}
+    for i in range(len(names)):
+        obj = ModelObject(names[i], descriptions[i])
+        objectlist[names[i]] = obj
+    return objectlist
+
+def load_initials_1(spec, objlist):
+    '''!
+    Function to add initial values from model specification into 
+    the table of objects. This function is used for AdvanceSyn 
+    model specification type 1.
+
+    @param spec Object: ConfigParser object containing the processed 
+    model - from modelspec_reader() or specobj_reader() functions.
+    @param objlist Dictionary: Table of objects representing the 
+    entities / nodes of the model - from generate_object_list_X() 
+    function.
+    @return: Dictionary of objects where key is the object name 
+    and value is the object numbering.
+    '''
+    for name in objlist:
+        if name in spec['Initials']:
+            initial_value = spec['Initials'][name]
+            objlist[name].value['initial'] = initial_value
+        else:
+            objlist[name].value['initial'] = 0
+    return objlist
+
+def process_reactions_1(spec):
+    '''!
+    Function to generate a table (dictionary) of reactions. This 
+    function is used for AdvanceSyn model specification type 1.
+
+    @param spec Object: ConfigParser object containing the processed 
+    model - from modelspec_reader() or specobj_reader() functions.
+    @return: Dictionary of reactions where key is the reaction ID 
+    and value is a dictionary of sources, destinations, and rateEq 
+    (rate equation) - sources and destinations is a list of source 
+    and destination nodes / entities respectively.
+    '''
+    rlist = [x for x in spec['Reactions']]
+    reactions = {}
+    for ID in rlist:
+        rdata = spec['Reactions'][ID]
+        rdata = rdata.split('|')
+        movement = rdata[0].strip()
+        rateEq = rdata[1].strip()
+        movement = movement.split('->')
+        sources = movement[0].strip()
+        sources = sources.split('+')
+        sources = [s.strip() for s in sources]
+        destinations = movement[1].strip()
+        destinations = destinations.split('+')
+        destinations = [d.strip() for d in destinations]
+        reactions[ID] = {'sources': sources,
+                         'destinations': destinations,
+                         'rateEq': rateEq}
+    return reactions
+
+def load_reactions_1(reactions, objlist):
+    '''!
+    Function to load the table (dictionary) of reactions into the 
+    table of objects. This function is used for AdvanceSyn model 
+    specification type 1.
+
+    @param reactions Dictionary: Table of reactions - from 
+    process_reactions_X() function.
+    @param objlist Dictionary: Table of objects representing the 
+    entities / nodes of the model.
+    @return: Dictionary of objects where key is the object name 
+    and value is the object numbering.
+    '''
+    for ID in reactions:
+        sources = reactions[ID]['sources']
+        destinations = reactions[ID]['destinations']
+        rateEq = reactions[ID]['rateEq']
+        for s in sources:
+            if s != '':
+                try:
+                    obj = objlist[s]
+                    obj.outflux[ID] = rateEq
+                except KeyError: 
+                    print('%s is not found in Objects' % str(s))
+        for d in destinations:
+            if d != '':
+                try:
+                    obj = objlist[d]
+                    obj.influx[ID] = rateEq
+                except KeyError: 
+                    print('%s is not found in Objects' % str(s))
+    return objlist
+
+def load_asm_objects(spec):
+    '''!
+    Function to load values and reactions into model objects based 
+    on AdvanceSyn model specification type.
+
+    @param spec Object: Model specification in ConfigParser object.
+    @return: Dictionary of objects where key is the object name 
+    and value is the object numbering.
+    '''
+    if spec['Specification']['type'] == '1':
+        objlist = generate_object_list_1(spec)
+        objlist = load_initials_1(spec, objlist)
+        reactions = process_reactions_1(spec)
+        objlist = load_reactions_1(reactions, objlist)
+    return objlist
+
+def process_asm_model(modelfile):
+    '''!
+    General function to process the AdvanceSyn model specification 
+    file into a table of objects (representing the entities) based 
+    on the type of model specification file as defined within the 
+    file. Hence, this function is the is the function to call to get 
+    table of objects from model specification file.
+
+    @param modelfile String: Relative path to the AdvanceSyn model 
+    specification file.
+    @return: A tuple of (ConfigParser object containing the 
+    processed model, Dictionary of objects where key is the object 
+    name and value is the object numbering).
+    '''
+    spec = modelspec_reader(modelfile, 'extended')
+    objlist = load_asm_objects(spec)
+    return (spec, objlist)
+    
